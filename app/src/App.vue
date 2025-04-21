aeaa30b955c9735d2df4ab2630a38034e727c8e7
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-04-21 14:01:24 -0400

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,1 +1,22 @@
 ../supabaseClient
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const user = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('users').select()
+  user.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+  </ul>
+</template>
