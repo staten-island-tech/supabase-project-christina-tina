bf8d2849006cc23a8a44cab642f7db7576e2fe68
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-04-29 14:00:23 -0400

    ahre

--- app/src/components/UserProfile.vue
+++ app/src/components/UserProfile.vue
@@ -5,25 +5,8 @@
 </template>
 
 <script setup>
-import { supabase } from '@/lib/supabaseClient'
-import { reactive } from 'vue'
-import type { User } from '../types'
 
-const user = reactive<User>({
-  email: '',
-  password: '',
-  username: '',
-  currency: 0,
-  score: 0,
-  items: [],
-})
 
-const { data, error} = await supabase.from('users').select('username')
-if (error) {
-    console.error('Error fetching data', error)
-} else {
-    console.log('Data', Data)
-}
 </script>
 
 <style scoped>
--- app/src/composables/useProf.ts
+++ app/src/composables/useProf.ts
@@ -1,2 +1,24 @@
 //code for User profile
 //Import into UserProfile.vue Later 
+
+import { supabase } from '@/lib/supabaseClient'
+import { reactive } from 'vue'
+import type { User } from '../types'
+
+const user = reactive<User>({
+  email: '',
+  password: '',
+  username: '',
+  currency: 0,
+  score: 0,
+  items: [],
+})
+
+export function userProfile() {
+    const { data, error} = await supabase.from("users").select(`username`)
+    if (error) {
+        console.error('Error fetching data', error)
+    } else {
+        console.log('Data', data)
+    }
+}
