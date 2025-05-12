9e03684a4d1fdddc0ea772e2501f0bc1b7aaf871
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-05-12 13:50:44 -0400

    gfdh

--- app/src/components/UserProfile.vue
+++ app/src/components/UserProfile.vue
@@ -1,3 +1,33 @@
+<template>
+  <form class="form-widget" @submit.prevent="updateProfile">
+    <div>
+      <label for="email">Email</label>
+      <input id="email" type="text" :value="session.user.email" disabled />
+    </div>
+    <div>
+      <label for="username">Name</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="website">Website</label>
+      <input id="website" type="url" v-model="website" />
+    </div>
+
+    <div>
+      <input
+        type="submit"
+        class="button primary block"
+        :value="loading ? 'Loading ...' : 'Update'"
+        :disabled="loading"
+      />
+    </div>
+
+    <div>
+      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
+    </div>
+  </form>
+</template>
+
 <script setup>
 import { supabase } from '../supabase'
 import { onMounted, ref, toRefs } from 'vue'
@@ -6,9 +36,15 @@ const props = defineProps(['session'])
 const { session } = toRefs(props)
 
 const loading = ref(true)
+const users = ref([])
 const username = ref('')
 const website = ref('')
 const avatar_url = ref('')
+
+async function getProfile(){
+  const { data } = await supabase.from('users').select()
+  users.value = data
+}
 
 onMounted(() => {
   getProfile()
@@ -74,33 +110,3 @@ async function signOut() {
   }
 }
 </script>
-
-<template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" :value="session.user.email" disabled />
-    </div>
-    <div>
-      <label for="username">Name</label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="website">Website</label>
-      <input id="website" type="url" v-model="website" />
-    </div>
-
-    <div>
-      <input
-        type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
-        :disabled="loading"
-      />
-    </div>
-
-    <div>
-      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
-    </div>
-  </form>
-</template>
