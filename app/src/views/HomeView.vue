a85c800e1c0c24ab9b8c4c0275fe669ca199fa66
christina <158211764+christinac09@users.noreply.github.com>
2025-05-19 13:31:24 -0400

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts">
-import SignoutButton from '../components/SignoutButton.vue'
-</script>
-
 <template>
   <main>
     <h1>hello this is the home page</h1>
     <SignoutButton v-if="store.isSignedIn" />
   </main>
 </template>
+
+<script setup>
+import SignoutButton from '../components/SignoutButton.vue'
+import { ref } from 'vue'
+</script>
