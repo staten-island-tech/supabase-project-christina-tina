afe899a39ad9be8e0954cb6adcd71c365d140d28
christina <158211764+christinac09@users.noreply.github.com>
2025-05-27 14:00:02 -0400

    drawer

--- app/src/supabaseClient.ts
+++ app/src/supabaseClient.ts
@@ -4,4 +4,5 @@ import type { SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase: SupabaseClient<any> = createClient(supabaseUrl, supabaseAnonKey)
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -53,9 +53,39 @@
       </div>
     </div>
     <h1 class="text-center text-4xl font-bold mt-10">hello this is the home page</h1>
-    <div class="routers">
-      <button class="btn">Start Game</button>
-      <button class="btn">View Stats</button>
+    <div class="routers absolute right-4 bottom-4">
+      <button class="btn btn-xl">Start Game</button>
+      <button class="btn btn-xl">View Stats</button>
+    </div>
+
+    <div class="drawer">
+      <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <!-- Page content here -->
+        <label for="my-drawer" class="btn btn-ghost btn-square drawer-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block h-6 w-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          <!-- Sidebar content here -->
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
