3d94bb8cbd568386e2e0c9e7a050e949fb92859e
christina <158211764+christinac09@users.noreply.github.com>
2025-06-06 14:00:32 -0400

    profile

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,71 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import LogoutBtn from './components/LogoutBtn.vue'
-import { useStore } from './stores/user'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-const store = useStore()
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header class="absolute top-0 left-0 w-full">
-    <div class="drawer">
-      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col">
-        <div class="navbar bg-base-300">
-          <div class="flex-none">
-            <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block h-6 w-6 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div class="mx-2 flex-1 px-2 text-center">
-            <a class="btn btn-ghost text-3xl">Title</a>
-          </div>
-          <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
-                <img alt="user profile picture" src="" />
-              </div>
-            </div>
-            <ul
-              tabindex="0"
-              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Profile</a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><LogoutBtn v-if="store.isSignedIn" /></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="drawer-side">
-        <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu bg-base-200 min-h-full w-80 p-4 text-xl">
-          <li><RouterLink to="/">Home</RouterLink></li>
-          <li><RouterLink to="/signin">Signin</RouterLink></li>
-          <li><RouterLink to="/profile">Profile</RouterLink></li>
-          <li><a>View Stats</a></li>
-          <li><a>something</a></li>
-          <li><a>Log Out</a></li>
-        </ul>
-      </div>
-    </div>
-  </header>
+  <header class="absolute top-0 left-0 w-full"><NavBar /></header>
 
   <RouterView />
 </template>
